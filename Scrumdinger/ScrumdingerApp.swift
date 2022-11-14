dbbb836cbdd8def97ad6da7a68795e4bd1a35e90
Alexander Spurlock <alexanderspurlock@gmail.com>
2022-11-13 19:29:20 -0500

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Alexander Spurlock on 11/13/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
